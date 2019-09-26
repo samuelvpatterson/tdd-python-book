7a994b8a7d5cd4de2552d4cb64faf34e80872c64
svp <samuelvpatterson@gmail.com>
2019-09-26 14:08:46 -0700

    Front page HTML now generated from a template

--- functional_tests.py
+++ functional_tests.py
@@ -29,16 +29,17 @@ class NewVisitorTest(unittest.TestCase):
 		)
 
 		# As a user I type a simple to do item
-		inputbox.sned_keys('Buy peacock feathers')
+		inputbox.send_keys('Buy peacock feathers')
 
 		# After hitting enter, I expect the numbered entry to refresh automatically
-		inputbox.sned_keys(Keys.ENTER)
+		inputbox.send_keys(Keys.ENTER)
 		time.sleep(1)
 
 		table = self.browser.find_element_by_id('id_list_table')
 		rows = table.find_elements_by_tag_name('tr')
 		self.assertTrue(
-			any(row.text == '1: Buy peacock feathers' for row in rows)
+			any(row.text == '1: Buy peacock feathers' for row in rows),
+			"New to-do item did not appear in table"
 		)
 		
 		# There remains a box to add another entry, I add a second
