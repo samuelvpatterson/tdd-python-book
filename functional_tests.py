5412fac5b8924688248b826394f054e89f61e383
svp <samuelvpatterson@gmail.com>
2019-10-03 16:58:54 -0700

    Refactor functional test to remove duplicated code

--- functional_tests.py
+++ functional_tests.py
@@ -11,6 +11,11 @@ class NewVisitorTest(unittest.TestCase):
 	def tearDown(self):
 		self.browser.quit()
 
+	def check_for_row_in_list_table(self, row_text):
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertIn(row_text, [row.text for row in rows])
+
 	def test_can_start_a_list_and_retrieve_it_later(self):
 
 		# As a user I'd like to see the home page of the to do list site
@@ -35,9 +40,7 @@ class NewVisitorTest(unittest.TestCase):
 		inputbox.send_keys(Keys.ENTER)
 		time.sleep(1)
 
-		table = self.browser.find_element_by_id('id_list_table')
-		rows = table.find_elements_by_tag_name('tr')
-		self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+		self.check_for_row_in_list_table('1: Buy peacock feathers')
 
 		# As a user I want to add a second item to my list
 		inputbox = self.browser.find_element_by_id('id_new_item')
@@ -46,10 +49,8 @@ class NewVisitorTest(unittest.TestCase):
 		time.sleep(1)
 
 		# The page updates again and shows the second item on the list
-		table = self.browser.find_element_by_id('id_list_table')
-		rows = table.find_elements_by_tag_name('tr')
-		self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
-		self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in rows])
+		self.check_for_row_in_list_table('1: Buy peacock feathers')
+		self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 		
 		# The site generates a unique URL per user to retain the stored list and explains this to the user
 		self.fail('Finish the test!')
